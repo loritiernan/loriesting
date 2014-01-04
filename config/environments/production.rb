f5eadeb07ad5dd0a49bfc150ede48b76f525c2b1
loritiernan <loritiernan@gmail.com>
2014-01-04 13:26:11 -0500

    implented devise for user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Loriesting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+#required for heroku
+#remember to change to live url
+   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
